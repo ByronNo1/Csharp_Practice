02e441d03d8ecd27026584d6929095bdaf20eb0a
Byron <luchiahobyron@gmail.com>
2023-07-17 22:29:41 +0800

    20230716

---
+++ 006/01-FORM/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _01_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHideOPEN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(414, 132);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(498, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnExit.ForeColor = System.Drawing.Color.Crimson;
+            this.btnExit.Location = new System.Drawing.Point(42, 572);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(421, 109);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHideOPEN
+            // 
+            this.btnHideOPEN.BackColor = System.Drawing.Color.Coral;
+            this.btnHideOPEN.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnHideOPEN.ForeColor = System.Drawing.Color.Crimson;
+            this.btnHideOPEN.Location = new System.Drawing.Point(42, 773);
+            this.btnHideOPEN.Name = "btnHideOPEN";
+            this.btnHideOPEN.Size = new System.Drawing.Size(421, 109);
+            this.btnHideOPEN.TabIndex = 3;
+            this.btnHideOPEN.Text = "Hide & Show";
+            this.btnHideOPEN.UseVisualStyleBackColor = false;
+            this.btnHideOPEN.Click += new System.EventHandler(this.btnHideOPEN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 1024);
+            this.Controls.Add(this.btnHideOPEN);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHideOPEN;
+    }
+}
+
---
+++ 006/01-FORM/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+            MessageBox.Show("Hello World");
+            Console.WriteLine("------- Hello World ------");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           textBox1.Text =  "Hello World";
+            button2.Text = textBox1.Text;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           // this.Close();
+           // this.Hide();
+
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form1_Load : Hello World");
+         
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show(" Form1_FormClosing : Hello World");
+
+        }
+
+        private void btnHideOPEN_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            //this.Visible = false;
+            Application.DoEvents();
+            Thread.Sleep(100);
+            this.Show();
+            //this.Visible = true;
+
+        }
+
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible)
+            {
+                MessageBox.Show(" Form1_VisibleChanged to True ");
+            }
+           
+        }
+
+
+    }
+
+}
